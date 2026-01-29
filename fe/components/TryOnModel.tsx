7ff1559359e0adb535c190b19d548482d563d039
gordonrampay <whyboo2004@gmail.com>
2026-01-30 03:02:01 +0700

    update fe+be 30/01

---
+++ fe/components/TryOnModel.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useGLTF } from "@react-three/drei";
+import { useEffect } from "react";
+import * as THREE from "three";
+
+export default function TryOnModel({
+  shirtPath,
+  color,
+}: {
+  shirtPath: string;
+  color: string;
+}) {
+  const human = useGLTF("/models/human.glb");
+  const shirt = useGLTF(shirtPath);
+
+  useEffect(() => {
+    // đổi màu áo
+    shirt.scene.traverse((child: any) => {
+      if (child.isMesh) {
+        child.material = child.material.clone();
+        child.material.color = new THREE.Color(color);
+      }
+    });
+
+    // căn áo lên thân người (fake try-on)
+    shirt.scene.position.set(0, 1.05, 0.02);
+    shirt.scene.scale.set(1.05, 1.05, 1.05);
+  }, [shirtPath, color]);
+
+  return (
+    <group>
+      <primitive object={human.scene} />
+      <primitive object={shirt.scene} />
+    </group>
+  );
+}
