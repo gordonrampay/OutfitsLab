5cf4b98a0da63e98804e1a04fd88f4f7565015b8
nguyenvancong19 <son123xoai@gmail.com>
2026-02-06 14:22:01 +0700

    update api report

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,5 +1,12 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+require('dotenv').config();
+
+const password = process.env.DB_PASSWORD || '';
 
 @Module({
   imports: [
@@ -8,11 +15,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 5432,
       username: 'postgres',
-      password: '123456',
+      password: password,
       database: 'outfitslab_db',
       autoLoadEntities: true,
-      synchronize: true, 
+      synchronize: true,
     }),
   ],
 })
-export class AppModule {}
+export class AppModule { }
