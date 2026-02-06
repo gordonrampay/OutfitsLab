f6293d599a9922333c7f637d979d1049d90d3b8c
gordonrampay <whyboo2004@gmail.com>
2026-02-06 14:11:18 +0700

    update module user,auth 06/02

---
+++ be/src/modules/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register(
+        {
+            secret : 'OutfitsLab_Key',
+            signOptions : {expiresIn : '1d'}
+        }
+    )
+],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
